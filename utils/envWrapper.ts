ef77b9d0dcf7c3e5819f5dfad81e29b5aae3e1d5
suvegesrebeka <suvegesrebeka@gmail.com>
2026-01-12 15:10:41 +0100

    qa: env vars and env warpper

--- utils/envWrapper.ts
+++ utils/envWrapper.ts
@@ -1,4 +1,19 @@
 export const env = {
-    uiBaseUrl: process.env.uiBaseUrl,
-    apiBaseUrl: process.env.apiBaseUrl
+    uiBaseUrl: process.env.uiBaseUrl as string,
+    homePageUrl: process.env.homePageUrl as string,
+    apiBaseUrl: process.env.apiBaseUrl as string,
+    users: {
+        standard: {
+            username: process.env.STANDARD_USER_USERNAME as string,
+            password: process.env.STANDARD_USER_PASSWORD as string
+        },
+        locked: {
+            username: process.env.LOCKED_USER_USERNAME as string,
+            password: process.env.LOCKED_USER_PASSWORD as string
+        },
+        invalid: {
+            username: process.env.INVALID_USER_USERNAME as string,
+            password: process.env.INVALID_USER_PASSWORD as string
+        }
+    }
 }
