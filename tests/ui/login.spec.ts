6734b6867be4260a549271c23e7fdf07a846520c
suvegesrebeka <suvegesrebeka@gmail.com>
2026-01-12 15:11:19 +0100

    qa: ui-test: login

---
+++ tests/ui/login.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+import { env } from '../../utils/envWrapper';
+import { LoginPage } from '../../pages/swag-labs/LoginPage';
+
+test.describe('Login Feature', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto(env.uiBaseUrl);
+    });
+
+    test('Successful login with standard user', async ({ page }) => {
+        const landingPageUrl = env.uiBaseUrl + env.homePageUrl;
+        const { username, password } = env.users.standard;
+        const loginPage = new LoginPage(page);
+
+        await loginPage.login(username, password);
+        await expect(page).toHaveURL(landingPageUrl);
+    });
+
+    test('Login fails with invalid password', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        const { username, password } = env.users.invalid;
+
+        await loginPage.login(username, password);
+        await loginPage.expectLoginErrorMessage('INVALID_CREDENTIALS')
+        await expect(page).toHaveURL(env.uiBaseUrl);
+    });
+
+    test('Locked out user cannot login', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        const { username, password } = env.users.locked;
+
+        await loginPage.login(username, password);
+        await loginPage.expectLoginErrorMessage('LOCKED_OUT');
+        await expect(page).toHaveURL(env.uiBaseUrl);
+    });
+
+});
---
+++ utils/loginErrorMessages.ts
@@ -0,0 +1,4 @@
+export const loginErrorMessages = {
+    LOCKED_OUT: 'locked out',
+    INVALID_CREDENTIALS: 'do not match any user',
+}
